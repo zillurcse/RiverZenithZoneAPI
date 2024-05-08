cdf57d0bf7e0c2cc8aa91eeb8c5f3abfcdb3277c
rakibmiah99 <rakibmiah120199@gmail.com>
2024-05-08 13:11:00 +0600

    f

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
-    return \App\Models\User::first()->createToken('admin token')->plainTextToken;
+//    return \App\Models\User::first()->createToken('admin token')->plainTextToken;
     return view('welcome');
 });
